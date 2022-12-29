5085b6983378352abcfcfac34a94990803998eba
Eugene Konstantinov <git@knst.me>
2022-12-29 16:58:45 +0300

    Changed gradient color

--- src/info/Info.js
+++ src/info/Info.js
@@ -8,7 +8,7 @@ import mock5 from "../img/mock5.png"
 /* Hi there! Thanks for checking out my portfolio template. Be sure to read the comments to get a better understanding of
 how to make this template work best for you! */
 
-export let colors = ["rgb(255, 0, 0)", "rgb(44, 242, 93)"];
+export let colors = ["rgb(24, 249, 219)", "rgb(71, 66, 223)"];
 /*
 I highly recommend using a gradient generator like https://gradientgenerator.paytonpierce.dev/ to generate a pair of colors that you like.
 These colors will be used to style your name on the homepage, the background of your picture, and some other accents throughout
