8f4500641b5b9d7d24785ffa981e012ff4aef826
Eugene Konstantinov <git@knst.me>
2023-01-14 11:53:19 +0300

    Updated terminal component for screenshots

--- src/components/about/Terminal.js
+++ src/components/about/Terminal.js
@@ -6,7 +6,10 @@ import {Box} from "@mui/material";
 const iconClass = "fa fa-circle";
 
 function Terminal(props) {
-   const {text} = props;
+   const {text, screenshot} = props;
+
+   const pyStyle = screenshot ? {} : {xs: '1rem', md: '2rem'};
+   const pxStyle = screenshot ? {} : {xs: '2rem', md: '3rem'};
 
    return (
       <Box component={'section'} className={classNames(Style.terminal, Style.shadowed)}
@@ -17,7 +20,7 @@ function Terminal(props) {
             <i className={classNames(iconClass, Style.amber)}/>
             <i className={classNames(iconClass, Style.green)}/>
          </Box>
-         <Box py={{xs: '1rem', md: '2rem'}} px={{xs: '2rem', md: '3rem'}} borderRadius={'0 0 0.5rem 0.5rem'}
+         <Box py={pyStyle} px={pxStyle} borderRadius={'0 0 0.5rem 0.5rem'}
               sx={{backgroundColor: '#27242f'}} fontSize={'1.5rem'} fontFamily={'Courier New, Courier, monospace'}>
             {text}
          </Box>
