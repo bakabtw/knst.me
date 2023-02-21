d4d3eea54bab2f3930bf13afd127569660238f22
Eugene Konstantinov <git@knst.me>
2023-02-21 17:50:37 +0300

    Updated initials

--- src/info/Info.js
+++ src/info/Info.js
@@ -21,7 +21,7 @@ of what each of the values mean.
 export const info = {
     firstName: "Eugene",
     lastName: "Konstantinov",
-    initials: "js", // the example uses first and last, but feel free to use three or more if you like.
+    initials: "ek", // the example uses first and last, but feel free to use three or more if you like.
     position: "a Full Stack Developer && Cloud DevOPS",
     selfPortrait: self, // don't change this unless you want to name your self-portrait in the "img" folder something else!
     gradient: `-webkit-linear-gradient(135deg, ${colors})`, // don't change this either
